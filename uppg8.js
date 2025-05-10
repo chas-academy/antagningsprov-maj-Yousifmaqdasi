6d5a9502914899be253b21b0951ed9da82ab88fa
Yousifmaqdasi <yousifmaqdasi@icloud.com>
2025-05-10 22:30:57 +0200

    Update uppg8.js

--- uppg8.js
+++ uppg8.js
@@ -10,8 +10,22 @@ function uppg8(){
 //  över 30 år som skrivs ut i konsolen
 
 // anropa funktionen och skicka med arrayen som argument
-
-
+  const personer = [
+    { name : "Alex", age: 24 },
+    { name : "Sara", age: 22 },
+    { name : "Emma", age: 28 },
+    { name : "Daniel", age: 34 },
+    { name : "Sandra", age: 40 }
+    ];
+  
+  function over30(personen){
+    for (let person of personen) {
+      if (person.age > 30){
+        console.log(person.name);
+      }
+    }
+}
+  over30(personer)
 }
 
 module.exports = { uppg8 };
