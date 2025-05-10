bb5254b6a0444f6ac12e0c264407412ab06510c2
Yousifmaqdasi <yousifmaqdasi@icloud.com>
2025-05-10 22:23:15 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -2,10 +2,10 @@
 
 function uppg6(){
 
-    // Skapa en array med namn som du döper till names
-    // Loopa igenom arrayen och skriv ut alla namn i konsolen
-
- 
+    let names = ["Yousif", "Adam", "Leo", "Emma"]
+    for (let i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
 }
 
 module.exports = { uppg6 };
