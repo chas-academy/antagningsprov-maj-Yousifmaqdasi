b61732f84d0151d90f6cf3d38e25be7a285d3ed4
Yousifmaqdasi <yousifmaqdasi@icloud.com>
2025-05-10 22:14:52 +0200

    Update uppg4.js

--- uppg4.js
+++ uppg4.js
@@ -1,12 +1,11 @@
 
 function uppg4() {
-  
-    // deklarera ett objekt med namnet person
-    // objektet ska ha två properties, name och age
-    // tilldela age 25
-    // skriv ut med dotnotation åldern i konsolen
+  const person = {
+    namn = "Yousif";
+    age = "22";
+};
 
-
+console.log(person.age);
 }
 
 module.exports = { uppg4 };
