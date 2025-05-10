5cf38d8c0caeaed17d6a4f54c9d6a720d3de176f
Yousifmaqdasi <yousifmaqdasi@icloud.com>
2025-05-10 22:13:05 +0200

    Update uppg3.js

--- uppg3.js
+++ uppg3.js
@@ -1,14 +1,10 @@
 
 
 function uppg3() {
-
-    // Skriv ut summan av två tal
-    // deklarera två variabler, a och b
-    // tilldela värdena 5 och 10 till variablerna
-    // skriv ut summan av a och b i konsolen
-
-
-
+    const a = 5;
+    const b = 10;
+    
+    console.log(a + b);
 }
 
 
