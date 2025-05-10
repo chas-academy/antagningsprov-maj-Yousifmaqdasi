f1ff9b5a773f07088d1ad30bd690dfe6c4b4ec00
Yousifmaqdasi <yousifmaqdasi@icloud.com>
2025-05-10 22:19:36 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -4,8 +4,15 @@ function uppg5() {
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
 
+    let förstaTal = 12;
+    let andraTal = 8;
 
-
+    if (förstaTal > andraTal){
+        console.log("Det första talet är större än det andra")
+    }
+        else{
+        console.log("Det andra talet är större än det första")
+    }
 }
 
 module.exports = { uppg5 };
